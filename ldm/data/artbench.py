7c1c7db2259d6b9d62ef5812564abfb1f77babd0
Peiyuan Liao <peiyuanl@cs.cmu.edu>
2022-06-04 02:05:24 -0700

    Update artbench.py

--- ldm/data/artbench.py
+++ ldm/data/artbench.py
@@ -1,11 +1,47 @@
 from torchvision.datasets import ImageFolder
+import numpy as np
+import albumentations
+class PrepDataset(ImageFolder):
+    def __init__(self, root, **kwargs):
+        self.size = size
+        self.random_crop = random_crop
+
+        self.labels = dict() if labels is None else labels
+        self.labels["file_path_"] = paths
+        self._length = len(paths)
+
+        if self.size is not None and self.size > 0:
+            self.rescaler = albumentations.SmallestMaxSize(max_size = self.size)
+            if not self.random_crop:
+                self.cropper = albumentations.CenterCrop(height=self.size,width=self.size)
+            else:
+                self.cropper = albumentations.RandomCrop(height=self.size,width=self.size)
+            self.preprocessor = albumentations.Compose([self.rescaler, self.cropper])
+        else:
+            self.preprocessor = lambda **kwargs: kwargs
+
+        super().__init__(root)
 
 
-class ArtBench10Train(ImageFolder):
+
+    def preprocess_image(self, image_path):
+        image = np.array(image).astype(np.uint8)
+        image = self.preprocessor(image=image)["image"]
+        image = (image/127.5 - 1.0).astype(np.float32)
+        return image
+
+    def __getitem__(self, i):
+        example = dict()
+        example["image"] = self.preprocess_image(super().__getitem__(i))
+        for k in self.labels:
+            example[k] = self.labels[k][i]
+        return example
+
+class ArtBench10Train(PrepDataset):
     def __init__(self, **kwargs):
-        super().__init__(root="data/artbench/train")
+        super().__init__(root="data/artbench/train", **kwargs)
 
 
-class ArtBench10Validation(ImageFolder):
+class ArtBench10Validation(PrepDataset):
     def __init__(self, **kwargs):
-        super().__init__(root="data/artbench/validation")
+        super().__init__(root="data/artbench/validation", **kwargs)
