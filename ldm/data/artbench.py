0d556624257d1b4e6cfaa19b6f91fadc87c80346
Peiyuan Liao <peiyuanl@cs.cmu.edu>
2022-06-04 00:58:34 -0700

    artbench

--- ldm/data/artbench.py
+++ ldm/data/artbench.py
@@ -3,9 +3,9 @@ from torchvision.datasets import ImageFolder
 
 class ArtBench10Train(ImageFolder):
     def __init__(self, **kwargs):
-        super().__init__(root="", **kwargs)
+        super().__init__(root="data/artbench/train", **kwargs)
 
 
 class ArtBench10Validation(ImageFolder):
     def __init__(self, **kwargs):
-        super().__init__(root="", **kwargs)
+        super().__init__(root="data/artbench/validation", **kwargs)
