8ecf84675bf72ef48e0767ddfc14dd185c1d6261
Xiuyu Li <xl289@cornell.edu>
2022-06-14 14:16:57 -0400

    add artbench unconditional impressionism

--- ldm/data/artbench.py
+++ ldm/data/artbench.py
@@ -1,7 +1,47 @@
 from torchvision.datasets import ImageFolder
 import numpy as np
 import albumentations
+import PIL
+from PIL import Image
+from torchvision import transforms
 class PrepDataset(ImageFolder):
+    def __init__(self, root, size=None, interpolation="bicubic", flip_p=0.5, **kwargs):
+        self.size = size
+        self.interpolation = {"linear": PIL.Image.LINEAR,
+                              "bilinear": PIL.Image.BILINEAR,
+                              "bicubic": PIL.Image.BICUBIC,
+                              "lanczos": PIL.Image.LANCZOS,
+                              }[interpolation]
+        self.flip = transforms.RandomHorizontalFlip(p=flip_p)
+
+        super().__init__(root)
+
+
+    def preprocess_image(self, image):
+        img = np.array(image).astype(np.uint8)
+        crop = min(img.shape[0], img.shape[1])
+        h, w, = img.shape[0], img.shape[1]
+        img = img[(h - crop) // 2:(h + crop) // 2,
+              (w - crop) // 2:(w + crop) // 2]
+        
+        image = Image.fromarray(img)
+        if self.size is not None:
+            image = image.resize((self.size, self.size), resample=self.interpolation)
+
+        image = self.flip(image)
+        image = np.array(image).astype(np.uint8)
+        image = (image / 127.5 - 1.0).astype(np.float32)
+        return image
+
+    def __getitem__(self, i):
+        example = dict()
+        ele, label = super().__getitem__(i)
+        example["image"] = self.preprocess_image(ele)
+        example["class_label"] = label
+        return example
+
+
+class PrepDatasetCond(ImageFolder):
     def __init__(self, root, size=None, random_crop=False, labels=None, **kwargs):
         self.size = size
         self.random_crop = random_crop
@@ -25,7 +65,7 @@ class PrepDataset(ImageFolder):
     def preprocess_image(self, image):
         image = np.array(image).astype(np.uint8)
         image = self.preprocessor(image=image)["image"]
-        image = (image/127.5 - 1.0).astype(np.float32)
+        image = (image / 127.5 - 1.0).astype(np.float32)
         return image
 
     def __getitem__(self, i):
@@ -37,11 +77,16 @@ class PrepDataset(ImageFolder):
         example["human_label"] = label
         return example
 
-class ArtBench10Train(PrepDataset):
+class ArtBench10Train(PrepDatasetCond):
     def __init__(self, **kwargs):
         super().__init__(root="data/artbench/train", **kwargs)
 
 
-class ArtBench10Validation(PrepDataset):
+class ArtBench10Validation(PrepDatasetCond):
     def __init__(self, **kwargs):
         super().__init__(root="data/artbench/validation", **kwargs)
+
+
+class ArtBench10Impressionism(PrepDataset):
+    def __init__(self, **kwargs):
+        super().__init__(root="data/artbench-impressionism", **kwargs)
